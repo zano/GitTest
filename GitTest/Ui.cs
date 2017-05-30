a48f374b068deafa38c0bbbc7f91165de7547298
Adrian Lozano <adrian@xenotype.com>
2017-05-30 16:55:53 +0200

    New features for Ui

--- GitTest/Ui.cs
+++ GitTest/Ui.cs
@@ -11,6 +11,8 @@ namespace GitTest
         public void Hello() {
             Console.WriteLine("Hello git!");
             Console.WriteLine("Hello again");
+
+            Console.WriteLine("New features");
         }
     }
 }
